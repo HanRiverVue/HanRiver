ba57665cbe98cb4e7b95aa116387a42376610857
wl990@naver.com <wl990@naver.com>
2025-01-21 02:36:41 +0900

    feat : 푸터 레이아웃 퍼블리싱

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,13 +1,37 @@
 <script setup>
+import UpScroll from '@/components/UpScroll.vue';
 import FooterLayout from '@/layout/FooterLayout.vue';
 import HeaderLayout from '@/layout/HeaderLayout.vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { RouterView } from 'vue-router';
+
+const isScrollTopShow = ref(false);
+const handlePageScroll = () => {
+  console.log(window.scrollY, window.innerHeight);
+  if (window.scrollY + window.innerHeight > window.innerHeight) {
+    isScrollTopShow.value = true;
+  } else {
+    isScrollTopShow.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handlePageScroll);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handlePageScroll);
+});
 </script>
 
 <template>
-  <HeaderLayout />
-  <main class="max-w-[1200px] px-10 mx-auto mt-20">
-    <RouterView></RouterView>
+  <main :class="$route.meta.bg_color">
+    <section class="max-w-[1200px] px-10 mx-auto mt-20">
+      <article class="h-[3000px]">
+        <HeaderLayout />
+        <RouterView></RouterView>
+        <UpScroll v-if="isScrollTopShow" />
+        <FooterLayout v-if="$route.meta.showFooter" />
+      </article>
+    </section>
   </main>
-  <FooterLayout v-if="$route.meta.showFooter" />
 </template>
