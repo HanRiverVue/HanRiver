4124408520b0a5e8937dbc45dc3abb73174523f8
AAminha <dks4857@gmail.com>
2025-01-22 00:09:43 +0900

    🔨 refactor: 파일명 수정

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,7 @@
-import CancelIcon from './cancel-icon.vue';
+import CancelIcon from './cancel_icon.vue';
 import bookmark_icon from './bookmark.svg';
-import cheering_icon from './cheering-icon.svg';
-import LinkIcon from './link-icon.vue';
-import PencilIcon from './pencil-icon.vue';
-import PlusIcon from './plus-icon.vue';
+import LinkIcon from './link_icon.vue';
+import PencilIcon from './pencil_icon.vue';
+import PlusIcon from './plus_icon.vue';
 
-export { bookmark_icon, cheering_icon, CancelIcon, LinkIcon, PencilIcon, PlusIcon };
+export { bookmark_icon, CancelIcon, LinkIcon, PencilIcon, PlusIcon };
