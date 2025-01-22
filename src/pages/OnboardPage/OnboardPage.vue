34ecf5d72e7c6208cc4b54e2df8e07598a999978
yoonji <kyjkbw@gmail.com>
2025-01-22 09:57:58 +0900

    💄 style: 온보딩 페이지 퍼블리싱 진행중

---
+++ src/pages/OnboardPage/OnboardPage.vue
@@ -0,0 +1,31 @@
+<script setup>
+import onboardLeft from '@/assets/images/onboard_left.png';
+import logo from '@/assets/icons/logo.svg';
+import OnboardStep1 from './components/OnboardStep1.vue';
+</script>
+
+<template>
+  <div class="w-full h-screen bg-secondary-3">
+    <div class="relative flex justify-between max-w-[1200px] h-full py-8 px-10 text-gray-80 m-auto">
+      <!-- 좌측 -->
+      <div>
+        <div class="mt-[19px] mb-[22px]">
+          <img :src="logo" alt="mergi 로고 아이콘" class="min-w-[96px]" />
+        </div>
+        <div>
+          <h3 class="mb-1 h3-b">탐색의 시작은 간단한 소개에서!</h3>
+          <p>프로젝트의 첫걸음을 위해 닉네임과 희망 직무를 설정해주세요.</p>
+        </div>
+        <img class="absolute bottom-8 left-10 w-[365px]" :src="onboardLeft" alt="온보딩 이미지" />
+      </div>
+      <!-- 우측 -->
+      <OnboardStep1 />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  border: 1px solid red;
+}
+</style>
---
+++ src/pages/OnboardPage/components/OnboardStep1.vue
@@ -0,0 +1,21 @@
+<script setup>
+import IntroduceInput from '@/components/IntroduceInput.vue';
+import NicknameInput from '@/components/NicknameInput.vue';
+</script>
+<template>
+  <div class="min-w-[574px] h-full bg-white/85 z-10">
+    <div class="w-full text-center h-9">바가 들어갈 자리</div>
+    <section>
+      <div class="h4-b text-gray-80">닉네임을 입력해주세요.</div>
+      <NicknameInput />
+    </section>
+    <div class="h4-b text-gray-80">닉네임을 입력해주세요.</div>
+    <BaseInput
+      :value="shortIntroduce"
+      placeholder="한 줄 소개"
+      :maxLength="MAX_SHORT_INTRODUCE_LENGTH"
+      @input="handleShortIntroduceInput"
+    />
+  </div>
+</template>
+<style scoped></style>
