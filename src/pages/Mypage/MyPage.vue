8d2c42da7ca07a5a39fc1f73fce30bfc580dff9b
hyunw00 <khwland090@gmail.com>
2025-01-22 12:19:23 +0900

    ✨ Feat: 내정보 탭  기본 기능 구현

--- src/pages/Mypage/MyPage.vue
+++ src/pages/Mypage/MyPage.vue
@@ -8,9 +8,40 @@ import MyRecruitmentList from './components/MyRecruitmentList.vue';
 import MyRequestList from './components/MyRequestList.vue';
 import ProfileEdit_icon from '@/assets/icons/profileEdit_icon.svg';
 import MyBookmark from './components/MyBookmark.vue';
+import { useRouter } from 'vue-router';
+import default_img from '@/assets/images/temp-profile.png';
 
 const items = ref(['내 정보', '작성한 모집글', '신청 목록', '찜 목록']);
 const activeIndex = ref(0);
+
+// 더미데이터 로그인 API 연결시 삭제 예정
+const dummyObj = {
+  id: 113,
+  short_introduce: '프론트엔드 개발자를 준비중인 김현우입니다!',
+  created_at: '2025-01-19T13:03:44.076158+00:00',
+  long_introduce: `아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 
+  왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 
+  왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~
+  아무노래나 막틀어~ 왜들 그리 다운돼 있어~아무노래나 막틀어~ 왜들 그리 다운돼 있어~`,
+  profile_img_path: null,
+  link: ['https://github.com/kodingzz'],
+  user_id: 'c21c4cc8-a05e-4370-994a-305e5b26faf4',
+  name: '혀누',
+  positions: [
+    {
+      id: 76,
+      position: '프론트엔드',
+      stacks: ['React', 'TypeScript', 'Next.js'],
+    },
+    {
+      id: 77,
+      position: '백엔드',
+      stacks: ['Node.js', 'Firebase', 'Spring'],
+    },
+  ],
+};
+
+const router = useRouter();
 </script>
 
 <template>
@@ -21,17 +52,22 @@ const activeIndex = ref(0);
     >
       <!-- 프로필 이미지 -->
       <div>
-        <img :src="basicProfoile" alt="프로필 이미지" />
+        <img
+          :src="dummyObj.profile_img_path ? dummyObj.profile_img_path : default_img"
+          alt="프로필 이미지"
+          class="w-[165px] h-[165px] rounded-full"
+        />
       </div>
       <!-- 프로필 정보 -->
       <div class="flex flex-col items-start gap-[16px] flex-1">
         <div class="flex flex-col items-start flex-1 w-full gap-1">
-          <p class="h2-b text-black">병알이</p>
+          <p class="h2-b text-black">{{ dummyObj.name }}</p>
 
           <div class="flex justify-between items-center w-full">
-            <p class="body-large-r text-gray-40">안녕하세요. 2년차 프론트 개발자입니다!</p>
+            <p class="body-large-r text-gray-40">{{ dummyObj.short_introduce }}</p>
 
             <div
+              @click="router.push('/editprofile')"
               class="flex py-1.5 px-3 justify-center items-center gap-1.5 rounded-[4px] bg-primary-1 hover:bg-primary-hover cursor-pointer"
             >
               <img :src="ProfileEdit_icon" alt="프로필 편집 아이콘(톱니바퀴)" class="w-4 h-4" />
@@ -61,10 +97,10 @@ const activeIndex = ref(0);
       />
       <!-- 탭 내용 -->
       <div>
-        <div v-if="activeIndex === 0"><MyInfo /></div>
-        <div v-else-if="activeIndex === 1"><MyRecruitmentList /></div>
-        <div v-else-if="activeIndex === 2"><MyRequestList /></div>
-        <div v-else-if="activeIndex === 3"><MyBookmark /></div>
+        <div v-if="activeIndex === 0"><MyInfo :userInfo="dummyObj" /></div>
+        <div v-else-if="activeIndex === 1"><MyRecruitmentList :userInfo="dummyObj" /></div>
+        <div v-else-if="activeIndex === 2"><MyRequestList :userInfo="dummyObj" /></div>
+        <div v-else-if="activeIndex === 3"><MyBookmark :userInfo="dummyObj" /></div>
       </div>
     </div>
   </div>
