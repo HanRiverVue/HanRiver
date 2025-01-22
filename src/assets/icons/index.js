a106f13a9048f115faa2102af1bbbf055c90bd20
AAminha <dks4857@gmail.com>
2025-01-22 10:27:17 +0900

    🔨 refactor: SearchInput 컴포넌트 파일 위치 변경 및 디자인 수정

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,11 @@
-import CancelIcon from './cancel_icon.vue';
+// svg
 import bookmark_icon from './bookmark.svg';
+
+// vue
+import CancelIcon from './cancel_icon.vue';
 import LinkIcon from './link_icon.vue';
 import PencilIcon from './pencil_icon.vue';
 import PlusIcon from './plus_icon.vue';
+import SearchIcon from './search_icon.vue';
 
-export { bookmark_icon, CancelIcon, LinkIcon, PencilIcon, PlusIcon };
+export { bookmark_icon, CancelIcon, LinkIcon, PencilIcon, PlusIcon, SearchIcon };
