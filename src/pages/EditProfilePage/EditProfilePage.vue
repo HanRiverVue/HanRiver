1bd314469fa518b89a6da4c4f7ae8b5c900aa982
AAminha <dks4857@gmail.com>
2025-01-23 17:55:58 +0900

    🔨 refactor: 불필요한 로직 코드 삭제

--- src/pages/EditProfilePage/EditProfilePage.vue
+++ src/pages/EditProfilePage/EditProfilePage.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { MAX_POSITION_COUNT, POSITION } from '@/constants';
-import { reactive, onMounted } from 'vue';
+import { onMounted } from 'vue';
 
 import ProfileLinks from './components/ProfileLinks.vue';
 import ProfilePositions from './components/ProfilePositions.vue';
@@ -8,41 +7,29 @@ import ProfileIntroduction from './components/ProfileIntroduction.vue';
 import ProfileSkills from './components/ProfileSkills.vue';
 import DefaultInformation from './components/DefaultInformation.vue';
 import { useRouter } from 'vue-router';
+import { useProfileStore } from '@/stores/profile';
 
 const router = useRouter();
+const profileStore = useProfileStore();
 
-const selectedPositions = reactive([]);
-const selectedSkills = reactive([]);
-
-const handleSelectPositions = (position) => {
-  if (selectedPositions.includes(position)) {
-    if (selectedPositions.length === 1) return;
-
-    const index = selectedPositions.indexOf(position);
-    selectedPositions.splice(index, 1);
-    selectedSkills.splice(index, 1);
-  } else {
-    if (selectedPositions.length === MAX_POSITION_COUNT) return;
-    selectedPositions.push(position);
-    selectedSkills.push([]);
-  }
+const handleCancel = () => {
+  router.push('/MyPage');
 };
 
-const handleCancel = () => router.push('/MyPage');
-
-const handleSubmit = () => {
+const handleSubmit = ($event) => {
+  $event.preventDefault();
   // 닉네임 중복 체크 여부 확인해야 함
+  if (!profileStore.isCheckNickname) {
+    alert('닉네임 중복 확인을 해주세요.');
+    return;
+  }
 };
 
 onMounted(() => {
   // TODO: 유저 정보를 받아서 초기화
   // TODO: 링크의 경우, id 삽입한 객체 배열로 변경
-
   // TODO: 수파베이스 연결 시, 삭제해야 할 코드
   // 절대 아무것도 없는 케이스를 안 만들기 위한 임시 로직
-  if (selectedPositions.length === 0) {
-    handleSelectPositions(POSITION[0]);
-  }
 });
 </script>
 
@@ -55,7 +42,7 @@ onMounted(() => {
       <ProfileIntroduction />
       <ProfileLinks />
       <ProfilePositions />
-      <ProfileSkills :selectedPositions="selectedPositions" />
+      <ProfileSkills />
     </section>
     <section class="flex items-center justify-end gap-3">
       <button type="button" class="secondary-button px-6 py-1.5 body-m" @click="handleCancel">
