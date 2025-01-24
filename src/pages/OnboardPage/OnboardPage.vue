8843eefd06575f1ff720a07be3e29f98587513c7
yoonji <kyjkbw@gmail.com>
2025-01-24 11:12:57 +0900

    ✨ feat: 온보딩 개발 중

--- src/pages/OnboardPage/OnboardPage.vue
+++ src/pages/OnboardPage/OnboardPage.vue
@@ -5,14 +5,50 @@ import OnboardStep1 from './components/OnboardStep1.vue';
 import OnboardStep2 from './components/OnboardStep2.vue';
 import AppButton from '@/components/AppButton.vue';
 
-import { onMounted, reactive, ref } from 'vue';
-const registerData = reactive({}); // 회원가입 데이터
+import { computed, reactive, ref } from 'vue';
+const registerData = reactive({
+  name: '',
+  short_introduce: '',
+  position: [], // 빈 배열로 초기화
+});
+
 const step = ref(1); // 현재 온보딩 단계
 
+// 폼 데이터 받기
 const setRegisterData = (newData) => {
   Object.assign(registerData, newData);
   console.log(JSON.stringify(registerData));
 };
+
+// 선택된 포지션
+const handlePositionsSelect = (value) => {
+  console.log('받아짐', value);
+  if (registerData.position.includes(value)) {
+    // 이미 선택된 포지션이면 제거
+    const index = registerData.position.indexOf(value);
+    registerData.position.splice(index, 1);
+  } else if (registerData.position.length < 3) {
+    // 선택 가능 개수를 초과하지 않으면 추가
+    registerData.position.push(value);
+  } else {
+    alert('최대 3개의 포지션만 선택 가능합니다.');
+  }
+};
+
+// 폼 유효성 검사
+const isFormValid = computed(() => {
+  return (
+    //TODO: 닉네임 중복 검사 추가
+    registerData.name.length > 0 &&
+    registerData.short_introduce.length > 0 &&
+    registerData.position.length > 0
+  );
+});
+
+// 버튼 타입 결정
+const buttonType = computed(() => {
+  return isFormValid.value ? 'primary' : 'disabled';
+});
 
 // 온보딩 단계 초기화
 step.value = 1;
@@ -27,13 +63,30 @@ step.value = 1;
         <div class="flex flex-col gap-7">
           <ProgressBar :currentPage="step" :maxPage="2" />
           <template v-if="step === 1">
-            <OnboardStep1 @update="setRegisterData" />
+            <OnboardStep1
+              :register-data="registerData"
+              @positions-select="handlePositionsSelect"
+              @update="setRegisterData"
+            />
           </template>
           <template v-if="step === 2">
             <OnboardStep2 />
           </template>
         </div>
-        <AppButton text="다음" class="py-[14px] body-large-m rounded-lg" />
+
+        <!-- TODO: 버튼 컴포넌트 내에 넣기 -->
+        <!-- 스탭 1 버튼 -->
+        <AppButton :type="buttonType" text="다음" class="py-[14px] body-large-m rounded-lg" />
+
+        <!-- 스탭 2 버튼 -->
+        <div class="flex gap-2">
+          <AppButton
+            type="secondary"
+            text="다음"
+            class="flex-1 py-[14px] body-large-m rounded-lg"
+          />
+          <AppButton type="primary" text="다음" class="flex-1 py-[14px] body-large-m rounded-lg" />
+        </div>
       </div>
     </div>
   </div>
--- src/pages/OnboardPage/components/OnboardStep1.vue
+++ src/pages/OnboardPage/components/OnboardStep1.vue
@@ -1,39 +1,75 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
 import BaseInput from '@/components/BaseInput.vue';
 import PositionSelectButton from '@/components/PositionSelectButton.vue';
 import { MAX_SHORT_INTRODUCE_LENGTH, MAX_NICKNAME_LENGTH, POSITION } from '@/constants';
+import { getAllUserInfo } from '@/api/supabase/user';
 
-const emit = defineEmits(['update']);
+const props = defineProps({
+  registerData: {
+    type: Object,
+  },
+});
+const emit = defineEmits(['update', 'positionsSelect']);
 
-const nickname = ref(''); //닉네임
-const shortIntroduce = ref(''); //한 줄 소개
-const selectedPositions = reactive([]); // 포지션
+const NicknameStatus = {
+  EMPTY: 'EMPTY', // 공백일 경우
+  INITIAL: 'INITIAL', // 초기 상태
+  VALID: 'VALID', // 사용 가능한 닉네임
+  DUPLICATE: 'DUPLICATE', // 중복된 닉네임
+  INVALID: 'INVALID', // 특수문자 등 규칙 위반
+};
+
+const NicknameMessages = {
+  EMPTY: '닉네임을 입력해주세요.',
+  INITIAL: '중복된 이름ㆍ특수문자 사용불가',
+  VALID: '사용 가능한 닉네임입니다',
+  DUPLICATE: '이미 사용 중인 닉네임입니다',
+  INVALID: '특수문자는 사용할 수 없습니다',
+};
+
+const NicknameMessageStyles = {
+  EMPTY: 'text-gray-50',
+  INITIAL: 'text-gray-50',
+  VALID: 'text-primary-3',
+  DUPLICATE: 'text-accent-error',
+  INVALID: 'text-accent-error',
+};
+
+const nicknameCheckResult = ref(NicknameStatus.INITIAL); // 닉네임 상태
+
+const NicknameMessage = computed(() => NicknameMessages[nicknameCheckResult.value]);
+const NicknameMessageStyle = computed(() => NicknameMessageStyles[nicknameCheckResult.value]);
 
 const handleNickNameInput = (value) => {
-  nickname.value = value;
   emit('update', { name: value });
 };
 
 const handleShortIntroduceInput = (value) => {
-  shortIntroduce.value = value;
   emit('update', { short_introduce: value });
 };
-const handlePositionsSelect = (value) => {
-  console.log(value);
-  if (selectedPositions.includes(value)) {
-    // 이미 선택된 포지션이면 제거
-    const index = selectedPositions.indexOf(value);
-    selectedPositions.splice(index, 1);
-  } else if (selectedPositions.length < 3) {
-    // 선택 가능 개수를 초과하지 않으면 추가
-    selectedPositions.push(value);
-  } else {
-    alert('최대 3개의 포지션만 선택 가능합니다.');
-  }
 
-  // 배열 형태로 업데이트 이벤트 발행
-  emit('update', { position: [...selectedPositions] });
+const handlePositionsSelect = (value) => {
+  emit('positionsSelect', value);
+};
+
+// 닉네임 유효성 검사
+const nicknameValidationStatus = async (nickname) => {
+  if (!nickname.trim()) {
+    // 공백일 경우
+    nicknameCheckResult.value = NicknameStatus.EMPTY;
+    return;
+  }
+  const allUserInfo = await getAllUserInfo(); // 모든 유저 정보 가져오기
+  const isDuplicate = allUserInfo.some((user) => user.name === nickname);
+  if (isDuplicate) {
+    nicknameCheckResult.value = NicknameStatus.DUPLICATE;
+  } else if (!/^[a-zA-Z0-9ㄱ-ㅎ가-힣\s]*$/.test(nickname)) {
+    // 특수문자 체크
+    nicknameCheckResult.value = NicknameStatus.INVALID;
+  } else {
+    nicknameCheckResult.value = NicknameStatus.VALID; // 유효한 닉네임
+  }
 };
 </script>
 
@@ -44,21 +80,25 @@ const handlePositionsSelect = (value) => {
       <section>
         <h3 class="h4-b text-gray-80 mb-[10px]">닉네임을 입력해주세요.</h3>
         <BaseInput
-          :value="nickname"
+          :value="registerData.name"
           placeholder="닉네임"
           :maxLength="MAX_NICKNAME_LENGTH"
           className="pr-2 py-2"
           @input="handleNickNameInput"
         >
           <template #rightIcon>
-            <button type="button" class="primary-button px-3 py-1.5 shrink-0 body-r">
+            <button
+              type="button"
+              class="primary-button px-3 py-1.5 shrink-0 body-r"
+              @click="nicknameValidationStatus(registerData.name)"
+            >
               중복확인
             </button>
           </template>
         </BaseInput>
         <div class="flex justify-between gap-3 mt-1 caption-r text-gray-50">
-          <p>중복된 이름ㆍ특수문자 사용불가</p>
-          <p>{{ nickname.length }}/{{ MAX_NICKNAME_LENGTH }}</p>
+          <p :class="NicknameMessageStyle">{{ NicknameMessage }}</p>
+          <p>{{ registerData.name.length }}/{{ MAX_NICKNAME_LENGTH }}</p>
         </div>
       </section>
 
@@ -66,14 +106,14 @@ const handlePositionsSelect = (value) => {
       <section>
         <h3 class="h4-b text-gray-80 mb-[10px]">간단하게 본인을 소개해주세요.</h3>
         <BaseInput
-          :value="shortIntroduce"
+          :value="registerData.short_introduce"
           placeholder="한 줄 소개"
           :maxLength="MAX_SHORT_INTRODUCE_LENGTH"
           @input="handleShortIntroduceInput"
         />
-        <p class="mt-1 body-r text-gray-50 justify-self-end">
-          {{ shortIntroduce.length }}/{{ MAX_SHORT_INTRODUCE_LENGTH }}
-        </p>
+        <div class="mt-1 body-r text-gray-50 justify-self-end">
+          <p>{{ registerData.short_introduce.length }}/{{ MAX_SHORT_INTRODUCE_LENGTH }}</p>
+        </div>
       </section>
 
       <!-- 포지션 -->
@@ -87,7 +127,7 @@ const handlePositionsSelect = (value) => {
             v-for="name in POSITION"
             :key="name"
             size="large"
-            :isSelected="selectedPositions.includes(name)"
+            :isSelected="props.registerData.position.includes(name)"
             @click="handlePositionsSelect(name)"
           >
             {{ name }}
